76779ea070f48c0e783af54d938f81e859bbf299
yumi1103 <tanisi.1103@gmail.com>
2020-12-24 02:05:30 +0900

    strage API追加

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -11,7 +11,9 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://3k5moy5eurcslfrsehvtnkmcpa.appsync-api.ap-northeast-1.amazonaws.com/graphql",
     "aws_appsync_region": "ap-northeast-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-t4cm6gaj6nhtzio65z36y76tbe"
+    "aws_appsync_apiKey": "da2-t4cm6gaj6nhtzio65z36y76tbe",
+    "aws_user_files_s3_bucket": "amplify-strage-backet201222-staging",
+    "aws_user_files_s3_bucket_region": "ap-northeast-1"
 };
 
 
